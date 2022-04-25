5e1cd88c5d6060e5b4c8d2b222a16e0ca785431b
ALmazDD <63290667+ALmazDD@users.noreply.github.com>
2022-04-26 00:41:41 +0600

    jsx

--- client/src/utils/consts.jsx
+++ client/src/utils/consts.jsx
@@ -3,5 +3,4 @@ export const LOGIN_ROUTE = '/login'
 export const REGISTRATION_ROUTE = '/registration'
 export const CTF_ROUTE = '/'
 export const TOSOLVE_ROUTE = '/toSolve'
-export const CHALLENGE_ROUTE = '/challenge'
- 
+export const CHALLENGE_ROUTE = '/challenge' 
